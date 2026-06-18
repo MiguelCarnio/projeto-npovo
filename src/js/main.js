d2e6425fd6af5e11f3930308ccbccfd7eae6cd5f
MiguelCarnio <miguel.carnio01@gmail.com>
2026-06-18 01:08:44 -0300

    sla amno

--- src/js/main.js
+++ src/js/main.js
@@ -16,39 +16,3 @@ botaoVoltarAoTopo.addEventListener('click', () => {
   });
 });
 
-const opcoesObservador = {
-  threshold: 0.1,
-  rootMargin: '0px 0px -100px 0px'
-};
-
-const observador = new IntersectionObserver((entradas) => {
-  entradas.forEach(entrada => {
-    if (entrada.isIntersecting) {
-      entrada.target.classList.add('animar-entrada');
-      observador.unobserve(entrada.target);
-    }
-  });
-}, opcoesObservador);
-
-document.addEventListener('DOMContentLoaded', () => {
-  const artigos = document.querySelectorAll('article, #apresentacao');
-  artigos.forEach(artigo => {
-    observador.observe(artigo);
-  });
-});
-
-document.querySelectorAll('a[href^="#"]').forEach(ancora => {
-  ancora.addEventListener('click', function (e) {
-    e.preventDefault();
-    const alvo = document.querySelector(this.getAttribute('href'));
-    if (alvo) {
-      alvo.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      });
-    }
-  });
-});
-
-
-
